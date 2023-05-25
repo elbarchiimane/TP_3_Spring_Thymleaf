2474fa1741bec9d1eb6c0c78b76f04428e20a643
Imane Elbarchi <elbarchiimane@gmail.com>
2023-05-25 12:16:26 +0100

    last commit

--- TP_Spring_MVC_Thymeleaf/src/main/java/com/example/tp_spring_mvc_thymeleaf/TpSpringMvcThymeleafApplication.java
+++ TP_Spring_MVC_Thymeleaf/src/main/java/com/example/tp_spring_mvc_thymeleaf/TpSpringMvcThymeleafApplication.java
@@ -6,6 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import java.util.Date;
 
@@ -22,5 +27,26 @@ public class TpSpringMvcThymeleafApplication implements CommandLineRunner {
     patientRepository.save(new Patient(null,"Mohamed",new Date(),false,200));
     patientRepository.save(new Patient(null,"Hanane",new Date(),true,600));
     patientRepository.save(new Patient(null,"Imane",new Date(),false,287));
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return  new BCryptPasswordEncoder();
+    }
+    //@Bean
+    CommandLineRunner commandLineRunner(JdbcUserDetailsManager jdbcUserDetailsManager){
+         PasswordEncoder passwordEncoder=passwordEncoder();
+         return args -> {
+            jdbcUserDetailsManager.createUser(
+                    User.withUsername("user2").password(passwordEncoder.encode("12345")).roles("USER").build()
+            );
+            jdbcUserDetailsManager.createUser(
+                User.withUsername("imane").password(passwordEncoder.encode("12345")).roles("USER").build()
+            );
+             jdbcUserDetailsManager.createUser(
+                     User.withUsername("admin").password(passwordEncoder.encode("12345")).roles("USER","ADMIN").build()
+             );
+
+        };
     }
 }
